98434004140da9b79f187b9d55b8a2048b9335b6
James Alley <manalagi001@yahoo.com>
2012-06-25 17:34:43 -0700

    Julien, this attempts to rectify a problem with mobile devices-- the mousedown event is not fired for some reason. So, I listen for touchstart as appropriate. Grep for the DISMISS_EVENT. Also did some extremely minor JSLinting.

--- src/gallery-simple-menu/js/simple-menu.js
+++ src/gallery-simple-menu/js/simple-menu.js
@@ -1,98 +1,102 @@
-/*
- * Copyright (c) 2010 Julien Lecomte. All rights reserved.
- * http://www.julienlecomte.net/
- */
+    'use strict';
 
-/**
- * Simple Menu plugin.
- * @module simple-menu
- */
+    /*
+     * Copyright (c) 2010 Julien Lecomte. All rights reserved.
+     * http://www.julienlecomte.net/
+     */
 
-var ACTIVE_CLASS_NAME = 'menu-active',
-    VISIBLE_CLASS_NAME = 'menu-visible',
-    ARIA_ROLE = 'role',
-    ARIA_HIDDEN = 'aria-hidden',
-    MENU_OPEN = 0,
-    MENU_CLOSED = 1;
+    /**
+     * Simple Menu plugin.
+     * @module simple-menu
+     */
 
-function SimpleMenu (config) {
-    this._link = config.host;
-    this._menu = this._link.next();
+    var ACTIVE_CLASS_NAME = 'menu-active',
+        VISIBLE_CLASS_NAME = 'menu-visible',
+        ARIA_ROLE = 'role',
+        ARIA_HIDDEN = 'aria-hidden',
+        MENU_OPEN = 0,
+        MENU_CLOSED = 1,
+        DISMISS_EVENT = (Y.config.win.hasOwnProperty && Y.config.win.hasOwnProperty('ontouchstart')) ? 'touchstart' : 'mousedown';
 
-    // Set ARIA attributes
+    function SimpleMenu(config) {
+        this._link = config.host;
+        this._menu = this._link.next();
 
-    this._link.set('aria-haspopup', 'true');
+        // Set ARIA attributes
 
-    this._menu.setAttrs({
-        'role': 'menu',
-        'aria-labelledby': this._link.getAttribute('id'),
-        'aria-hidden': 'true'
-    });
+        this._link.set('aria-haspopup', 'true');
 
-    this._menu.all('ul,li').set(ARIA_ROLE, 'presentation');
-    this._menu.all('a').set(ARIA_ROLE, 'menuitem');
+        this._menu.setAttrs({
+            'role': 'menu',
+            'aria-labelledby': this._link.getAttribute('id'),
+            'aria-hidden': 'true'
+        });
 
-    // Setup keyboard navigation.
+        this._menu.all('ul,li').set(ARIA_ROLE, 'presentation');
+        this._menu.all('a').set(ARIA_ROLE, 'menuitem');
 
-    this._menu.plug(Y.Plugin.NodeFocusManager, {
-        descendants: 'a',
-        keys: (Y.UA.gecko ? { previous: 'press:38', next: 'press:40' } :
-                { previous: 'down:38', next: 'down:40' })
-    });
+        // Setup keyboard navigation.
 
-    // Open/close the menu.
+        this._menu.plug(Y.Plugin.NodeFocusManager, {
+            descendants: 'a',
+            keys: (Y.UA.gecko ? { previous: 'press:38', next: 'press:40' } :
+                    { previous: 'down:38', next: 'down:40' })
+        });
 
-    this._link.on('click', function (e) {
-        e.halt();
-        this.toggle();
-    }, this);
+        // Open/close the menu.
 
-    this._menu.on('keydown', function (e) {
-        if (e.keyCode === 27 /* Esc */) {
+        this._link.on('click', function (e) {
             e.halt();
-            this.hide();
-        }
-    }, this);
+            this.toggle();
+        }, this);
 
-    Y.one(document).on('mousedown', function (e) {
-        var target = e.target;
+        this._menu.on('keydown', function (e) {
+            if (e.keyCode === 27) {
+                /* Esc */
+                e.halt();
+                this.hide();
+            }
+        }, this);
 
-        if (target !== this._link && !this._menu.contains(target)) {
-            this.hide();
-        }
-    }, this);
-}
+        Y.one(document).on(DISMISS_EVENT, function (e) {
+            var target = e.target;
+            if (target !== this._link && !this._menu.contains(target)) {
+                this.hide();
+            }
+        }, this);
 
-SimpleMenu.prototype = {
-
-    _state: MENU_CLOSED,
-
-    show: function () {
-        if (this._state !== MENU_OPEN) {
-            this._link.addClass(ACTIVE_CLASS_NAME);
-            this._menu.addClass(VISIBLE_CLASS_NAME);
-            this._menu.set(ARIA_HIDDEN, false);
-            this._menu.focusManager.focus();
-            this._state = MENU_OPEN;
-        }
-    },
-
-    hide: function () {
-        if (this._state !== MENU_CLOSED) {
-            this._link.removeClass(ACTIVE_CLASS_NAME);
-            this._menu.removeClass(VISIBLE_CLASS_NAME);
-            this._menu.set(ARIA_HIDDEN, true);
-            this._link.focus();
-            this._state = MENU_CLOSED;
-        }
-    },
-
-    toggle: function () {
-        this[this._state === MENU_CLOSED ? 'show' : 'hide']();
     }
-};
 
-SimpleMenu.NAME = 'SimpleMenu';
-SimpleMenu.NS = 'simplemenu';
+    SimpleMenu.prototype = {
 
-Y.namespace('Plugin').SimpleMenu = SimpleMenu;
+        _state: MENU_CLOSED,
+
+        show: function () {
+            if (this._state !== MENU_OPEN) {
+                this._link.addClass(ACTIVE_CLASS_NAME);
+                this._menu.addClass(VISIBLE_CLASS_NAME);
+                this._menu.set(ARIA_HIDDEN, false);
+                this._menu.focusManager.focus();
+                this._state = MENU_OPEN;
+            }
+        },
+
+        hide: function () {
+            if (this._state !== MENU_CLOSED) {
+                this._link.removeClass(ACTIVE_CLASS_NAME);
+                this._menu.removeClass(VISIBLE_CLASS_NAME);
+                this._menu.set(ARIA_HIDDEN, true);
+                this._link.focus();
+                this._state = MENU_CLOSED;
+            }
+        },
+
+        toggle: function () {
+            this[this._state === MENU_CLOSED ? 'show' : 'hide']();
+        }
+    };
+
+    SimpleMenu.NAME = 'SimpleMenu';
+    SimpleMenu.NS = 'simplemenu';
+
+    Y.namespace('Plugin').SimpleMenu = SimpleMenu;
